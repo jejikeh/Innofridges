510ddb389f75b908dbf72b9a4df416bebd8bbe4e
Yauheni Lantsau <jejikeh@gmail.com>
2022-10-22 12:26:15 +0300

    Add to pet class PetAge field

--- InnoGotchi.Backend/Core/InnoGotchi.Domain/Pet/Pet.cs
+++ InnoGotchi.Backend/Core/InnoGotchi.Domain/Pet/Pet.cs
@@ -2,13 +2,33 @@
 
 public class Pet
 {
+    /// <summary>
+    /// Id of the user who owns this pet
+    /// </summary>
     public Guid UserId { get; set; }
     public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Pet name
+    /// </summary>
     public string Name { get; set; }
-    public string Age => _petAge.Age;
+    
+    /// <summary>
+    /// Creation time
+    /// </summary>
+    public DateTime BornDate { get; set; }
+    
+    /// <summary>
+    /// Pet age
+    /// </summary>
+    public string Age => CalculateAge(); 
     public PetStatus.Hunger Hunger { get; }
     public PetStatus.Thirsty Thirsty { get; }
     public int HappienessDayCount { get; }
-
-    private PetAge _petAge = new PetAge();
+    
+    private string CalculateAge()
+    {
+        var daysFromBorn = DateTime.Now.Subtract(BornDate);
+        return daysFromBorn > TimeSpan.FromDays(7) ? (daysFromBorn.Days / 7).ToString() : "NEWBORN";
+    }
 }

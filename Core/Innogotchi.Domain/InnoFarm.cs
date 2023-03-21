86e09eeb68cfe246ea24b2d852ca777637bec129
jejikeh <jejikeh@gmail.com>
2023-03-21 20:09:26 +0300

    domain layer

---
+++ Core/Innogotchi.Domain/InnoFarm.cs
@@ -0,0 +1,8 @@
+﻿namespace Innogotchi.Domain;
+
+public class InnoFarm
+{
+    public Guid InnoFarmGuid { get; set; }
+    public string FarmName { get; set; } = "Innofarm";
+    public ICollection<InnoPet> InnoPets { get; set; }
+}
---
+++ Core/Innogotchi.Domain/InnoUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Innogotchi.Domain;
+
+public class InnoUser
+{
+    public Guid InnoUserId { get; set; }
+    public string Username { get; set; } = "Default";
+    public string Email { get; set; } = "default@noname.com";
+    public string PasswordHash { get; set; } = "";
+}
